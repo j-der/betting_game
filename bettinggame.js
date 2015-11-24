a47719ae761e8a609097da952b7e21cc7908eb08
Jas Der <jasmineder@gmail.com>
2015-11-24 23:50:42 +0000

    fixed up some validations on bet amount and number

--- bettinggame.js
+++ bettinggame.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   $('#gamble').on('click', function() {
 
@@ -12,42 +11,52 @@ $(document).ready(function() {
 
     var randomNumber = $('<p>The drawn number is ' + Math.floor(Math.random() * 10 + 1) + '.</p>');
 
-    $('#status_update').append(showPlayerGamble);
-    // $(this).after(showPlayerGamble);
 
-    $('#reveal').on('click', function() {
-      $('#show_number').append(randomNumber);
+    if(playerBet < 5 || playerBet > 10) {
+      alert("Please bet between $5 to $10.");
+    }
 
+    else if(playerGuess < 1 || playerGuess > 10) {
+      alert("Invalid guess. Please choose a number between 1 and 10.");
+    }
 
-      if(playerGuess == randomNumber) {
-        playerMoney = playerMoney + (playerBet * 2);
+    else {
 
-        var winAlert = $('<p>You doubled your bet! You now have $' + playerMoney + '</p>');
-      }
-      else if(playerGuess == (randomNumber + 1) || playerGuess == (randomNumber - 1)) {
-          playerMoney = playerMoney;
-          alert("Close! You keep the money you bet. You now have $" + playerMoney)
+      $('#status_update').append(showPlayerGamble);
+      // $(this).after(showPlayerGamble);
+
+      $('#reveal').on('click', function() {
+        $('#show_number').append(randomNumber);
+
+        if(playerGuess == randomNumber) {
+          playerMoney = playerMoney + (playerBet * 2);
+          var winAlert = $('<p>You doubled your bet! You now have $' + playerMoney + '</p>');
         }
-      else {
-        playerMoney = (playerMoney - playerBet);
-        alert("You lose. Gambling is dangerous. You now have $" + playerMoney)
-      }
-        $('#show_number').append('<button id=button2>Play again?</button>');
-        $('#button2').on('click', function(event){
-          $('#bet').val('');
-          $('#guess').val('');
-          $('#status_update').hide();
-          $('#show_number').hide();
-          console.log('button2 clicked!');
-        });
 
+        else if(playerGuess == (randomNumber + 1) || playerGuess == (randomNumber - 1)) {
+            playerMoney = playerMoney;
+            alert("Close! You keep the money you bet. You now have $" + playerMoney)
+          }
 
-    });
+        else {
+          playerMoney = (playerMoney - playerBet);
+          alert("You lose. Gambling is dangerous. You now have $" + playerMoney)
+        }
+
+          $('#show_number').append('<button id=button2>Play again?</button>');
+          $('#button2').on('click', function(event){
+            $('#bet').val('');
+            $('#guess').val('');
+            $('#status_update').hide();
+            $('#show_number').hide();
+            console.log('button2 clicked!');
+          });
+
+      });
+  }
 
   });
 
-    // alert("Uh-oh, you're broke!")
-  // });
 });
 
 
