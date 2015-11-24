095273c276c27afa55daf124bcc405cf20815dd5
Jas Der <jasmineder@gmail.com>
2015-11-24 23:37:20 +0000

    commit

--- bettinggame.js
+++ bettinggame.js
@@ -13,33 +13,38 @@ $(document).ready(function() {
     var randomNumber = $('<p>The drawn number is ' + Math.floor(Math.random() * 10 + 1) + '.</p>');
 
     $('#status_update').append(showPlayerGamble);
+    // $(this).after(showPlayerGamble);
 
     $('#reveal').on('click', function() {
-    $('#show_number').append(randomNumber);
+      $('#show_number').append(randomNumber);
 
 
-if(playerGuess == randomNumber) {
-      playerMoney = playerMoney + (playerBet * 2);
+      if(playerGuess == randomNumber) {
+        playerMoney = playerMoney + (playerBet * 2);
 
-
-      alert("You doubled your bet! You now have $" + playerMoney);
-    }
-    else if(playerGuess == (randomNumber + 1) || playerGuess == (randomNumber - 1)) {
-        playerMoney = playerMoney;
-        alert("Close! You keep the money you bet. You now have $" + playerMoney)
+        var winAlert = $('<p>You doubled your bet! You now have $' + playerMoney + '</p>');
       }
-    else {
-      playerMoney = (playerMoney - playerBet);
-      alert("You lose. Gambling is dangerous. You now have $" + playerMoney)
-    }
+      else if(playerGuess == (randomNumber + 1) || playerGuess == (randomNumber - 1)) {
+          playerMoney = playerMoney;
+          alert("Close! You keep the money you bet. You now have $" + playerMoney)
+        }
+      else {
+        playerMoney = (playerMoney - playerBet);
+        alert("You lose. Gambling is dangerous. You now have $" + playerMoney)
+      }
+        $('#show_number').append('<button id=button2>Play again?</button>');
+        $('#button2').on('click', function(event){
+          $('#bet').val('');
+          $('#guess').val('');
+          $('#status_update').hide();
+          $('#show_number').hide();
+          console.log('button2 clicked!');
+        });
+
 
     });
 
-
-
-
-
-    });
+  });
 
     // alert("Uh-oh, you're broke!")
   // });
